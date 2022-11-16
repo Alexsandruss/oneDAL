e0a6c245779080393c24b765578e439dc5dddaba
Alexander Andreev <alexander.andreev@intel.com>
2022-11-16 14:55:26 +0000

    Fix clang-win flags 4

--- dev/make/cmplr.clang.mk
+++ dev/make/cmplr.clang.mk
@@ -27,8 +27,6 @@ CORE.SERV.COMPILER.clang = generic
 
 -Zl.clang =
 -DEBC.clang = -g
--cxx11 = $(if $(OS_is_win),-std=c++14,-std=c++11)
--cxx17 = -std=c++17
 
 COMPILER.mac.clang = clang++ -m64 -fgnu-runtime -stdlib=libc++ -mmacosx-version-min=10.14 -fwrapv \
                      -Werror -Wreturn-type
